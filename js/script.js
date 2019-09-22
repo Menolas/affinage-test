ccafb53078ea6b0615e015986581527540ec0475
Olena Kunina <elena@Olenas-MacBook-Pro.local>
2019-09-22 10:51:26 +0300

    righthendler works

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@
 var images = [
 	{
 		alt: 'some picture',
-		url: 'img/photo-1.jpg'
+		url: 'file:///Users/elena/affinage-test/img/photo-1.jpg'
 	},
 
 	{
         alt: 'some picture',
-		url: 'img/photo-2.jpg'
+		url: 'file:///Users/elena/affinage-test/img/photo-2.jpg'
 	},
 
 	{
         alt: 'some picture',
-		url: 'img/photo-3.jpg'
+		url: 'file:///Users/elena/affinage-test/img/photo-3.jpg'
 	}
 ];
 
@@ -22,30 +22,31 @@ var leftHandler = document.querySelector('.control-panel__handler--left');
 var backBlock = document.querySelector('.gallery__block--back img');
 var frontBlock = document.querySelector('.gallery__block--front img');
 
+frontBlock.src = images[2].url;
+backBlock.src = images[0].url;
+
 rightHandler.addEventListener('click', function () {
-    var sweptPictureUrl = frontBlock.src;
-    for (var i = 0; i < images.length; i++) {
-    	if (sweptPictureUrl === images[i].url) {
-    		var j;
-    		if (i >= images.length - 1) {
-    			j = 0;
-    		} else {
-    		    j = i + 1;
-    	    }
-    		backBlock.src = sweptPictureUrl;
-    		frontBlock.src = images[j].url;
-    	}
-    }
+	var sweptPictureUrl = frontBlock.src;
+	for (var i = 0; i < images.length; i++) {
+		if (images[i].url === sweptPictureUrl) {
+			if (i < images.length - 1) {
+				var j = i + 1;
+			} else if (i = images.length - 1) {
+			    j = 0;
+			}
+			frontBlock.src = images[j].url;
+	        backBlock.src = sweptPictureUrl;
+		}
+	}
 });
 
 leftHandler.addEventListener('click', function () {
     var sweptPictureUrl = frontBlock.src;
     for (var i = 0; i < images.length; i++) {
-    	if (sweptPictureUrl === images[i].url) {
-    		var j;
+    	if (images[i].url === sweptPictureUrl) {
     		if (i = 0) {
-    			j = 2;
-    		} else {
+    			var j = 2;
+    		} else if (i > 0) {
     		    j = i - 1;
     	    }
     		backBlock.src = sweptPictureUrl;
