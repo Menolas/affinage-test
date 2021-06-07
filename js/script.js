8441b23bb7e85bf454da1acc61dd5a37f287e749
Elena Christensen <olenachristensen777@gmail.com>
2021-06-07 11:31:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@
 var images = [
 	{
 		alt: 'some picture',
-		url: 'file:///Users/elena/affinage-test/img/photo-1.jpg'
+		url: '/img/photo-1.jpg'
 	},
 
 	{
         alt: 'some picture',
-		url: 'file:///Users/elena/affinage-test/img/photo-2.jpg'
+		url: '/img/photo-2.jpg'
 	},
 
 	{
         alt: 'some picture',
-		url: 'file:///Users/elena/affinage-test/img/photo-3.jpg'
+		url: '/img/photo-3.jpg'
 	}
 ];
 
